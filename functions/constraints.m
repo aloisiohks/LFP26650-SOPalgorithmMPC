1177576445602890e20ea7b428dbc66bbeb8fd2f
Aloisio Kawakita <56883161+aloisiohks@users.noreply.github.com>
2021-09-21 21:06:04 -0600

    Add files via upload

---
+++ functions/constraints.m
@@ -0,0 +1,88 @@
+function [ M, gamma ] = constraints( dx, linMatrices, mpcData,uk,rflag)
+%CONSTRAINTS Summary of this function goes here
+%   Detailed explanation goes here
+% Cell current: icell = ig + Istr
+% tic
+    Np = mpcData.Np;
+    Nc = mpcData.Nc;
+    Sigma = tril(ones(Nc,Nc));
+    model=mpcData.model;
+    
+    Am = linMatrices.A;
+    Bm = linMatrices.B;
+   
+    
+    C_z = linMatrices.C_z;
+    D_z = linMatrices.D_z;
+    C_v = linMatrices.C_v;
+    D_v = linMatrices.D_v;
+    C_Tc = linMatrices.C_Tc;
+    D_Tc = linMatrices.D_Tc;
+    
+    
+    [Phi_Tc,G_Tc] = predMat(Am,Bm,C_Tc,D_Tc,Nc,Np);
+    [Phi_v,G_v] = predMat(Am,Bm,C_v,D_v,Nc,Np);
+    [Phi_z,G_z] = predMat(Am,Bm,C_z,D_z,Nc,Np);
+    
+
+     
+    OCV =  OCVfromSOCtemp(dx(1),dx(4),model); 
+    
+    %****************************************************************************        
+        % CHARGE
+    if strcmp(rflag,'charge')
+        umax = 0;
+        umin = mpcData.const.u_min;
+        
+        M = [
+                 % u constraints
+                 -Sigma;         % i  > i_min
+                  Sigma;         % i  < i_max
+                 % v constraints
+                    G_v;         % v  < v_max
+                 % Tc constraint
+                   G_Tc;        % Tc < Tc_max
+                  ] ;
+        
+        gamma = [ 
+            
+                % u constraints
+              -umin*ones(Nc,1) + uk*ones(Nc,1);
+               umax*ones(Nc,1) - uk*ones(Nc,1);
+                % v constraints
+               mpcData.const.v_max*ones(Np ,1) - Phi_v*dx - OCV*ones(Np,1) ;
+               % Tc constraint
+               mpcData.const.tc_max - Phi_Tc*dx ;
+                 ];
+        
+    end
+        
+%****************************************************************************        
+        % DISCHARGE
+   if strcmp(rflag,'discharge')    
+        
+        umax = mpcData.const.u_max;
+        umin = 0;
+         
+                M = [
+                   -Sigma;         % i  > i_min
+                    Sigma;         % i  < i_max
+                   -G_v  ;         % v  > v_min
+                    G_Tc;          % Tc < Tc_max 
+%                     -G_z;          % z > z_min
+                     ] ;
+        
+         gamma = [ 
+                 -umin*ones(Nc,1) + uk*ones(Nc,1);     % i  > i_min
+                  umax*ones(Nc,1) - uk*ones(Nc,1);     % i  < i_max
+                 -mpcData.const.v_min + Phi_v*dx  + OCV*ones(Np,1);   % v  > v_min
+                   mpcData.const.tc_max - Phi_Tc*dx ;      % Tc < Tc_max
+%                    -mpcData.const.z_min + Phi_z*dx;
+                    ];
+        
+        
+    end
+       
+
+end
+
---
+++ functions/iterMPC_chg.m
@@ -0,0 +1,116 @@
+
+%   iterMPC computes SOP charge at time k
+%
+%   xk_1: current state
+%   mpcData: mpcData configuration
+%   Nsim: prediction interval
+%   deltaT: sampling time
+
+
+function [Y,X,U,DelU,Pavg,Pinst,mpcData] = iterMPC_chg(xk_1,mpcData,Nsim,deltaT)
+
+
+    % Load MPC data
+    Np = mpcData.Np;
+    Nc = mpcData.Nc;
+    Tfk = mean(mpcData.Tfk);    
+    model = mpcData.model;
+    DelU = zeros(1,Nsim+1);
+    Ru  = mpcData.Ru;
+    Ref = mpcData.const.z_max*ones(Np,1);
+    uk = mpcData.const.u_min;
+    mpcData.uk_1 = uk;
+
+
+        
+
+if ((xk_1(1) < mpcData.const.z_max) && (xk_1(4) < (mpcData.const.tc_max )) )
+   
+   % Calculate intial state and output values
+   [v,x,~,~] = iterModel(xk_1, uk,Tfk, model,deltaT);
+   Y(:,1) = v;
+   Xf = [xk_1;Tfk; 1; sign(uk); uk];
+   X(:,1) = Xf;
+   U(:,1) = uk;
+   xold = xk_1;
+   u = uk;
+   
+   %*************************************************************************
+   % MAIN LOOP
+   for kk = 2:Nsim
+       
+    % Obtain SS matrices
+    [linMatrices] = linMat_Ts(xold,mpcData);
+    Az = linMatrices.A;
+    Bz = linMatrices.B;
+    Cz = linMatrices.C_z;
+    Dz = linMatrices.D_z;
+    mpcData.linMatrices = linMatrices;
+  
+   % Compute SOC prediction matrices
+   [Phi,G,~]  = predMat(Az,Bz,Cz,Dz,Nc,Np);
+      
+   F = -G'*(Ref - Phi*X(:,kk-1) );
+   if mpcData.adap == 1
+       [~,S,~] = svd(G'*G);
+       [m,n] = size(S);
+       Ru = (norm(F,2)/(2*mpcData.const.du_max*sqrt(Nc)))-(S(m,n)); 
+   end
+   E = (G'*G + Ru*eye(mpcData.Nc,mpcData.Nc)); 
+   
+   DU = -E\F;
+   
+    % Augmented state vector
+    dx = [xold; Tfk; 1; sign(u); u];
+   
+   [M,gamma] = constraints(dx,linMatrices,mpcData,u,'charge');
+   
+  
+    if sum(M*DU - gamma > 0) > 0
+        [DU,lambda,nexec] = hildreth(E,F,M,gamma,[],150);
+         mpcData.lambda = lambda;
+         mpcData.nexec = nexec;
+    else 
+        mpcData.nexec = 0;
+    end
+      
+    du = DU(1);
+    DelU(:,kk) = du;
+    u = du + U(:,kk-1) ;
+    U(:,kk) = u;
+  
+    [v,x,~,~] = iterModel(xold, u,Tfk, model,deltaT);
+    xold = x;
+    Y(:,kk) = v;
+    X(:,kk) = [x;Tfk; 1; sign(u) ;u];
+    mpcData.uk_1 = u;
+    mpcData.v = v;
+    mpcData.Ru_store(kk) = Ru;
+
+   end
+   %*************************************************************************
+   
+    %POWER COMPUTATION
+    Pinst = Y(2:Nsim).*U(1,2:Nsim);  % Compute instantaneous power over prediction horizon
+    Etot = trapz(Pinst);             % Compute corresponding total energy
+    Pavg = Etot/11;                   % Compute max (average) sustained power over interval
+
+
+else
+
+u = 0;
+[v,x,~,~] = iterModel(xk_1, uk,Tfk, model,deltaT);
+
+mpcData.Ru_store = repmat(0,1,Nsim);
+Y(:,1:Nsim) = repmat(v,1,Nsim);
+X(:,1:Nsim) = repmat([x; Tfk; 1; sign(uk); uk],1,Nsim);
+U(1:Nsim) = u*ones(1,Nsim);
+Pavg = 0;
+Pinst = [];    
+
+end
+
+%     toc
+%     disp('iterMPC')
+end
+
