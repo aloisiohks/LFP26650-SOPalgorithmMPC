2071ce700e4273f714abf6ec4f0e2420dbe34cda
Aloisio Kawakita <56883161+aloisiohks@users.noreply.github.com>
2022-08-13 22:29:23 -0600

    Update iterMPC_chg.m

--- functions/iterMPC_chg.m
+++ functions/iterMPC_chg.m
@@ -20,7 +20,7 @@ function [Y,X,U,DelU,Pavg,Pinst,mpcData] = iterMPC_chg(xk_1,mpcData,Nsim,deltaT)
     Ref = mpcData.const.z_max*ones(Np,1);
     uk = mpcData.const.u_min;
     mpcData.uk_1 = uk;
-
+    du_max = abs(mpcData.const.u_max);
 
         
 
@@ -54,7 +54,7 @@ if ((xk_1(1) < mpcData.const.z_max) && (xk_1(4) < (mpcData.const.tc_max )) )
    if mpcData.adap
        [~,S,~] = svd(G'*G);
        [m,n] = size(S);
-       Ru = abs((norm(F,2)/(2*mpcData.const.du_max*sqrt(Nc)))-(S(m,n)));
+       Ru = abs((norm(F,2)/(2*du_max*sqrt(Nc)))-(S(m,n)));
    end
    E = (G'*G + Ru*eye(mpcData.Nc,mpcData.Nc)); 
    
